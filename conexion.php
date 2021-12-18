4f717790df90e533767c4bc90c918916b789250d
maicolpm <96062065+maicolpm@users.noreply.github.com>
2021-12-18 16:30:12 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 	$hn="localhost";
 	$un="root";
 	$pw="";
-	$db="programacionweb";
+	$db="maicolprogramacionwebbd";
 	$port ="3360";
 	$conexion = mysqli_connect($hn, $un, $pw, $db, $port);
 	if($conexion->connect_error) die("fallo al conectar con la base de datos");
